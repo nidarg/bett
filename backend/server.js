6145741026899ee285e9116df825190d15aab9e1
Gradin <gradin_g@yahoo.com>
2026-03-25 13:41:29 +0200

    add port

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,19 @@ import subscribeRoutes from "./src/routes/subscribeRoutes.js"
 import webhookRoutes from "./src/routes/webhookRoutes.js"
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 
 app.use("/webhook", webhookRoutes)
 app.use(express.json())
+
 app.use("/api", subscribeRoutes)
 
 app.get("/", (req, res) => {
   res.send("API running")
 })
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
