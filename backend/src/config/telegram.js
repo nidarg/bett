edfd07baf7c68553667c82b3f9c50d56a52702be
Gradin <gradin_g@yahoo.com>
2026-03-31 15:02:38 +0200

    fix bot telegram

--- backend/src/config/telegram.js
+++ backend/src/config/telegram.js
@@ -1,3 +1,8 @@
 import TelegramBot from "node-telegram-bot-api"
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
+import TelegramBot from "node-telegram-bot-api"
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+  polling: true
+})
+
 export default bot
